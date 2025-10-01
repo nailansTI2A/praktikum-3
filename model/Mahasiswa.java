547867f4d8966db2c2cc90dfb4f7de602a687d32
nailans <nailanur870@gmail.com>
2025-10-01 15:38:53 +0700

    menambahkan kode baru Mahasiswa.java ke folder model

--- model/Mahasiswa.java
+++ model/Mahasiswa.java
@@ -1,7 +1,20 @@
 package model;
 
 public class Mahasiswa {
+    public String nama;
+    public float ipk;
 
-   public float ipk; 
+    public Mahasiswa(String nama, float ipk) {
+        this.nama = nama;
+        this.ipk = ipk;
+    }
 
+    public void tampil() {
+        System.out.println("Nama : " + nama + ", IPK : " + ipk);
+    }
+
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa("Naila", 3.75f);
+        mhs.tampil();
+    }
 }
