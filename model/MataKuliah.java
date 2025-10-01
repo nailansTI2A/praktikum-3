d671e9f3ffff2e3555950c5091016b4c4d9ffd84
nailans <nailanur870@gmail.com>
2025-10-01 15:35:14 +0700

    menambahkan kode baru MataKuliah.java ke folder model

--- model/MataKuliah.java
+++ model/MataKuliah.java
@@ -1,7 +1,20 @@
 package model;
 
 public class MataKuliah {
-
     public String namaMataKuliah;
-    
+    public int sks;
+
+    public MataKuliah(String nama, int sks) {
+        this.namaMataKuliah = nama;
+        this.sks = sks;
+    }
+
+    public void tampil() {
+        System.out.println(namaMataKuliah + " - " + sks + " SKS");
+    }
+
+    public static void main(String[] args) {
+        MataKuliah mk = new MataKuliah("Pemrograman Java", 3);
+        mk.tampil();
+    }
 }
